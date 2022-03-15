15f3ba89fde2e65ffd2f3551f80c49e20e7adedf
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-03-15 19:35:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,50 +12,50 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int k = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = len-1; j > i; j--) {
-      if (arr[j] > value - arr[i]) {
-        continue;
-        } else if (arr[j] < value - arr[i]) {
-          continue;
-        } else if (arr[j] == value - arr[i]) {
-          k++;
-        } else {
-          break;
-        }
-    }
-  }
-  return k;
+	int k = 0;
+	while (arr[len - 1] > value) {
+		len--;
+	}
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[i] + arr[j] == value) {
+				k++;
+			}
+		}
+	}
+	return k;
 }
 
-int cbinsearch(int* arr, int size, int number, int left) {
-int i = left, j = size, k = 0; // i - левая граница массива, j - правая
-while (i < j) {
-  int mid = i + (j - i) / 2;
-  if (arr[mid] == number) {
-    int x = mid;
-    k++;
-    while (arr[--mid] == number)
-      k++;
-    while (arr[++x] == number)
-      k++;
-    return k;
-  } else if (arr[mid] > number) {
-	  j = mid;
-  } else {
-	  i = mid + 1;
-  }
-}
-return 0;
+int search(int* arr, int len, int value, int left) {
+	int i = left, j = len - 1, k = 0; // i - левая граница массива, j - правая
+	while (i < j) {
+		int mid = i + (j - i) / 2;
+		if (arr[mid] == value && mid != left) {
+			k++;
+			int x = mid, xx = mid;
+			while (arr[++x] == value) {
+				k++;
+			}
+			while (arr[--xx] == value && xx > left) {
+				k++;
+			}
+			return k;
+		}
+		else if (arr[mid] > value)
+			j = mid;
+		else
+			i = mid + 1;
+	}
+	return 0;
 }
 
 int countPairs3(int *arr, int len, int value) {
   int k = 0;
-  for (int i = 0; i < len; i++) {
-    if (value - arr[i] >= arr[i]) {
-      k += cbinsearch(arr, len, value - arr[i], i);
-      }
-  }
-  return k;
+	for (int i = 0; i < len; i++)
+	{
+		k += search(arr, len, value - arr[i],i);
+	}
+	return k;
 }
