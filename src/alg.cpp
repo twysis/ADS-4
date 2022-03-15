0a7732ac32f3fbf466cc1abe692c76bdb93e7bdb
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-03-15 17:14:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,39 +14,40 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   int k = 0;
   for (int i = 0; i < len; i++) {
-    for (int j = len-1; j > 0; j--) {
-      if (arr[i] + arr[j] > value) {
+    for (int j = len-1; j > i; j--) {
+      if (arr[j] > value - arr[i]) {
         continue;
-      } else if (arr[i] + arr[j] == value) {
-        k++;
-      } else {
-        break;
-      }
+        } else if (arr[j] < value - arr[i]) {
+          continue;
+        } else if (arr[j] == value - arr[i]) {
+          k++;
+        } else {
+          break;
+        }
     }
   }
   return k;
 }
 
 int cbinsearch(int* arr, int len, int value) {
-	int i = 0, j = len - 1, k = 1;
-	while (i < j) {
-		int mid = i + (j - i) / 2;
-		if (arr[mid] == value) {
-			int x = mid;
-			while (arr[--mid] == value)
-				k++;
-			while (arr[++x] == value)
-				k++;
-			return k;
-		}
-		else if (arr[mid] > value) {
-			j = mid;
-		}
-		else {
-			i = mid + 1;
-		}
-	}
-	return 0;
+int i = left, j = size, k = 0; // i - левая граница массива, j - правая
+while (i < j) {
+  int mid = i + (j - i) / 2;
+  if (arr[mid] == number) {
+    int x = mid;
+    k++;
+    while (arr[--mid] == number)
+      k++;
+    while (arr[++x] == number)
+      k++;
+    return k;
+  } else if (arr[mid] > number) {
+	  j = mid;
+  } else {
+	  i = mid + 1;
+  }
+}
+return k;
 }
 
 int countPairs3(int *arr, int len, int value) {
