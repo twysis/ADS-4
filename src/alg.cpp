839c4c3e3a34089239730886ddc8ba70e59d3ab8
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-03-15 15:36:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int countPairs1(int *arr, int len, int value) {
   int k = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i+1; j < len; j++) {
-      if (arr[i] + arr[j] == 50) {
+      if (arr[i] + arr[j] == value) {
         k++;
       }
     }
@@ -17,7 +17,7 @@ int countPairs2(int *arr, int len, int value) {
     for (int j = len-1; j > 0; j--) {
       if (arr[i] + arr[j] > value) {
         continue;
-      } else if (arr[i] + arr[j] == 50) {
+      } else if (arr[i] + arr[j] == value) {
         k++;
       } else {
         break;
